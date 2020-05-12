93c71da66a44583a0e41551dad807e628f8cc025
buidhui <buidhui@gmail.com>
2020-05-12 22:58:11 +0700

    learning useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
+import ColorBox from "./components/ColorBox";
+import TodoList from "./components/ToDoList";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "Leaning react Hooks" },
+    { id: 2, title: "Start FreeCodeCamp" },
+    { id: 3, title: "Make a lunch" },
+    { id: 4, title: "Do something" },
+    { id: 5, title: "Go shopping" },
+    { id: 6, title: "Have a meal" },
+  ]);
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex((x) => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="App">
-      <h1>Welcome Hooks</h1>
+      <h1>Color Box</h1>
+      <ColorBox />
+
+      <h1>ToDo List</h1>
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
