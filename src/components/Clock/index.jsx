ae420a9293383b14e9098e5211ddfc9e2528758a
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-07 13:58:50 +0800

    Delete src directory

--- src/components/Clock/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import useClock from "../../Hooks/useClock";
-
-function Clock() {
-  const { time } = useClock();
-  return <p style={{ fontSize: "42px" }}>{time}</p>;
-}
-
-export default Clock;
